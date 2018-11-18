a9331cafd1b2e1e24651a7a8b7ba1c911fb4f946
Aykut Asil <aykuttasil@gmail.com>
2018-11-18 23:53:52 +0300

    update

--- app/src/main/java/aykuttasil/com/modernapp/di/components/AppComponent.kt
+++ app/src/main/java/aykuttasil/com/modernapp/di/components/AppComponent.kt
@@ -13,10 +13,6 @@ import dagger.android.AndroidInjector
 import dagger.android.support.AndroidSupportInjectionModule
 import javax.inject.Singleton
 
-
-/**
- * Created by aykutasil on 8.12.2017.
- */
 @Singleton
 @Component(modules = [
     (AndroidSupportInjectionModule::class),
@@ -29,27 +25,9 @@ interface AppComponent : AndroidInjector<App> {
 
     @Component.Builder
     interface Builder {
-
-        // @BindsInstance ile Application nesnesi bekleyen modüllerdeki fonksiyonlara zaten oluşturulmuş Application nesnesini bind ediyoruz
-        // Örnek: {@link AppModule.provideSharedPreference}
-        // https://proandroiddev.com/dagger-2-component-builder-1f2b91237856
-        // Bunu yapmak için modül de constructer olarak beklenen parametreyi (Application) fonksiyona taşımamız gerekli
         @BindsInstance
         fun application(application: Application): Builder
 
-        // Eğer modül constructerında bizden parametre beklemiyor ise Componenti oluştururken(DaggerAppComponent.Builder)
-        // bu modülü (AppModule) ayrı olarak tanımlamamıza gerek yok. Ve @BindsInstance kullanarak modül içerisinde aynı tipte (Application)
-        // parametre bekleyen fonksiyonlara, zaten oluşturulmuş olan nesneyi daggerın otomatik olarak atama yapmasını sağlarız.
-        // fun appModule(appModule: AppModule): Builder
-
         fun build(): AppComponent
-
     }
-
-    /*
-    // ----> or
-    @Component.Builder
-    internal abstract inner class Builder : AndroidInjector.Builder<App>()
-    */
-
 }
