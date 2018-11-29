14c2642064bedff2b9f306855d4d36e85e4b4338
Aykut Asil <aykuttasil@gmail.com>
2018-11-30 02:26:27 +0300

    update

--- app/src/test/java/aykuttasil/com/modernapp/ExampleUnitTest.kt
+++ app/src/test/java/aykuttasil/com/modernapp/ExampleUnitTest.kt
@@ -1,16 +1,20 @@
 package aykuttasil.com.modernapp
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@RunWith(JUnit4::class)
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
+
         assertEquals(4, 2 + 2)
     }
 }
