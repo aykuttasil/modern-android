4b234f43c501d2d09c39d6ae2d3ca0e74c288b2b
Aykut Asil <aykuttasil@gmail.com>
2019-03-16 22:26:45 +0300

    update

--- app/src/main/kotlin/aykuttasil/com/modernapp/ui/common/DataBoundListAdapter.kt
+++ app/src/main/kotlin/aykuttasil/com/modernapp/ui/common/DataBoundListAdapter.kt
@@ -32,21 +32,21 @@ abstract class DataBoundListAdapter<T, V : ViewDataBinding>(
   appExecutors: AppExecutors,
   diffCallback: DiffUtil.ItemCallback<T>
 ) : ListAdapter<T, DataBoundViewHolder<V>>(
-        AsyncDifferConfig.Builder<T>(diffCallback)
-                .setBackgroundThreadExecutor(appExecutors.diskIO())
-                .build()
+  AsyncDifferConfig.Builder<T>(diffCallback)
+    .setBackgroundThreadExecutor(appExecutors.diskIO())
+    .build()
 ) {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataBoundViewHolder<V> {
-        val binding = createBinding(parent)
-        return DataBoundViewHolder(binding)
-    }
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataBoundViewHolder<V> {
+    val binding = createBinding(parent)
+    return DataBoundViewHolder(binding)
+  }
 
-    protected abstract fun createBinding(parent: ViewGroup): V
+  protected abstract fun createBinding(parent: ViewGroup): V
 
-    override fun onBindViewHolder(holder: DataBoundViewHolder<V>, position: Int) {
-        bind(holder.binding, getItem(position))
-        holder.binding.executePendingBindings()
-    }
+  override fun onBindViewHolder(holder: DataBoundViewHolder<V>, position: Int) {
+    bind(holder.binding, getItem(position))
+    holder.binding.executePendingBindings()
+  }
 
-    protected abstract fun bind(binding: V, item: T)
+  protected abstract fun bind(binding: V, item: T)
 }
--- app/src/main/kotlin/aykuttasil/com/modernapp/ui/common/RetryCallback.kt
+++ app/src/main/kotlin/aykuttasil/com/modernapp/ui/common/RetryCallback.kt
@@ -15,9 +15,6 @@
  */
 package aykuttasil.com.modernapp.ui.common
 
-/**
- * Created by aykutasil on 2.03.2018.
- */
 interface RetryCallback {
-    fun retry()
+  fun retry()
 }
