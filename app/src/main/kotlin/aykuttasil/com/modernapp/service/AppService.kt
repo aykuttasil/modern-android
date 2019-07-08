0dcd2854a76eab11b32dd65dd6642342d535befc
Aykut Asil <aykuttasil@gmail.com>
2019-07-08 13:13:49 +0300

    DaggerService implementation

--- app/src/main/kotlin/aykuttasil/com/modernapp/service/AppService.kt
+++ app/src/main/kotlin/aykuttasil/com/modernapp/service/AppService.kt
@@ -18,8 +18,9 @@ package aykuttasil.com.modernapp.service
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import dagger.android.DaggerService
 
-class AppService : Service() {
+class AppService : DaggerService() {
 
   override fun onBind(p0: Intent?): IBinder? {
     return null
