c805c1e625742c705ea81d3709af50d6c17f5dae
Aykut Asil <aykuttasil@gmail.com>
2019-07-08 12:25:15 +0300

    code beauty

--- app/src/main/kotlin/aykuttasil/com/modernapp/di/AppInjector.kt
+++ app/src/main/kotlin/aykuttasil/com/modernapp/di/AppInjector.kt
@@ -70,7 +70,11 @@ object AppInjector {
 
     (activity as? FragmentActivity)?.supportFragmentManager?.registerFragmentLifecycleCallbacks(
       object : FragmentManager.FragmentLifecycleCallbacks() {
-        override fun onFragmentCreated(fm: FragmentManager, f: Fragment, savedInstanceState: Bundle?) {
+        override fun onFragmentCreated(
+          fm: FragmentManager,
+          f: Fragment,
+          savedInstanceState: Bundle?
+        ) {
           if (f is Injectable) {
             AndroidSupportInjection.inject(f)
           }
