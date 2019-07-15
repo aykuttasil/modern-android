c8a3186cbb0ad384e8486d510e4a4efc88fd6426
Aykut Asil <aykuttasil@gmail.com>
2019-07-16 02:45:04 +0300

    code beauty

--- network/src/main/java/com/aykutasil/network/ApiResponse.kt
+++ network/src/main/java/com/aykutasil/network/ApiResponse.kt
@@ -24,33 +24,33 @@ import java.util.regex.Pattern
 </T> */
 @Suppress("unused") // T is used in extending classes
 sealed class ApiResponse<T> {
-    companion object {
-        fun <T> create(error: Throwable): ApiErrorResponse<T> {
-            return ApiErrorResponse(error.message ?: "unknown error")
-        }
-
-        fun <T> create(response: Response<T>): ApiResponse<T> {
-            return if (response.isSuccessful) {
-                val body = response.body()
-                if (body == null || response.code() == 204) {
-                    ApiEmptyResponse()
-                } else {
-                    ApiSuccessResponse(
-                            body = body,
-                            linkHeader = response.headers().get("link")
-                    )
-                }
-            } else {
-                val msg = response.errorBody()?.string()
-                val errorMsg = if (msg.isNullOrEmpty()) {
-                    response.message()
-                } else {
-                    msg
-                }
-                ApiErrorResponse(errorMsg ?: "unknown error")
-            }
-        }
+  companion object {
+    fun <T> create(error: Throwable): ApiErrorResponse<T> {
+      return ApiErrorResponse(error.message ?: "unknown error")
     }
+
+    fun <T> create(response: Response<T>): ApiResponse<T> {
+      return if (response.isSuccessful) {
+        val body = response.body()
+        if (body == null || response.code() == 204) {
+          ApiEmptyResponse()
+        } else {
+          ApiSuccessResponse(
+            body = body,
+            linkHeader = response.headers().get("link")
+          )
+        }
+      } else {
+        val msg = response.errorBody()?.string()
+        val errorMsg = if (msg.isNullOrEmpty()) {
+          response.message()
+        } else {
+          msg
+        }
+        ApiErrorResponse(errorMsg ?: "unknown error")
+      }
+    }
+  }
 }
 
 /**
@@ -62,123 +62,45 @@ data class ApiSuccessResponse<T>(
   val body: T,
   val links: Map<String, String>
 ) : ApiResponse<T>() {
-    constructor(body: T, linkHeader: String?) : this(
-            body = body,
-            links = linkHeader?.extractLinks() ?: emptyMap()
-    )
+  constructor(body: T, linkHeader: String?) : this(
+    body = body,
+    links = linkHeader?.extractLinks() ?: emptyMap()
+  )
 
-    val nextPage: Int? by lazy(LazyThreadSafetyMode.NONE) {
-        links[NEXT_LINK]?.let { next ->
-            val matcher = PAGE_PATTERN.matcher(next)
-            if (!matcher.find() || matcher.groupCount() != 1) {
-                null
-            } else {
-                try {
-                    Integer.parseInt(matcher.group(1))
-                } catch (ex: NumberFormatException) {
-                    // Timber.w("cannot parse next page from %s", next)
-                    null
-                }
-            }
+  val nextPage: Int? by lazy(LazyThreadSafetyMode.NONE) {
+    links[NEXT_LINK]?.let { next ->
+      val matcher = PAGE_PATTERN.matcher(next)
+      if (!matcher.find() || matcher.groupCount() != 1) {
+        null
+      } else {
+        try {
+          Integer.parseInt(matcher.group(1))
+        } catch (ex: NumberFormatException) {
+          // Timber.w("cannot parse next page from %s", next)
+          null
         }
+      }
     }
+  }
 
-    companion object {
-        private val LINK_PATTERN = Pattern.compile("<([^>]*)>[\\s]*;[\\s]*rel=\"([a-zA-Z0-9]+)\"")
-        private val PAGE_PATTERN = Pattern.compile("\\bpage=(\\d+)")
-        private const val NEXT_LINK = "next"
+  companion object {
+    private val LINK_PATTERN = Pattern.compile("<([^>]*)>[\\s]*;[\\s]*rel=\"([a-zA-Z0-9]+)\"")
+    private val PAGE_PATTERN = Pattern.compile("\\bpage=(\\d+)")
+    private const val NEXT_LINK = "next"
 
-        private fun String.extractLinks(): Map<String, String> {
-            val links = mutableMapOf<String, String>()
-            val matcher = LINK_PATTERN.matcher(this)
+    private fun String.extractLinks(): Map<String, String> {
+      val links = mutableMapOf<String, String>()
+      val matcher = LINK_PATTERN.matcher(this)
 
-            while (matcher.find()) {
-                val count = matcher.groupCount()
-                if (count == 2) {
-                    links[matcher.group(2)] = matcher.group(1)
-                }
-            }
-            return links
+      while (matcher.find()) {
+        val count = matcher.groupCount()
+        if (count == 2) {
+          links[matcher.group(2)] = matcher.group(1)
         }
+      }
+      return links
     }
+  }
 }
 
 data class ApiErrorResponse<T>(val errorMessage: String) : ApiResponse<T>()
-
-/*
-class ApiResponse<T> {
-    val code: Int
-    val body: T?
-    val errorMessage: String?
-    val links: MutableMap<String, String>
-
-    constructor(error: Throwable) {
-        code = 500
-        body = null
-        errorMessage = error.message
-        links = ArrayMap<String, String>()
-    }
-
-    constructor(response: Response<T>) {
-        code = response.code()
-        if (response.isSuccessful) {
-            body = response.body()
-            errorMessage = null
-        } else {
-            var message: String? = null
-            if (response.errorBody() != null) {
-                try {
-                    message = response.errorBody()!!.string()
-                } catch (ignored: IOException) {
-                    LogUtils.error("error while parsing response")
-                }
-
-            }
-            if (message == null || message.trim { it <= ' ' }.isEmpty()) {
-                message = response.message()
-            }
-            errorMessage = message
-            body = null
-        }
-        val linkHeader = response.headers().get("link")
-        if (linkHeader == null) {
-            links = ArrayMap<String, String>()
-        } else {
-            links = ArrayMap<String, String>()
-            val matcher = LINK_PATTERN.matcher(linkHeader)
-
-            while (matcher.find()) {
-                val count = matcher.groupCount()
-                if (count == 2) {
-                    links.put(matcher.group(2), matcher.group(1))
-                }
-            }
-        }
-    }
-
-    val isSuccessful: Boolean
-        get() = code in 200..299
-
-    val nextPage: Int?
-        get() {
-            val next = links[NEXT_LINK] ?: return null
-            val matcher = PAGE_PATTERN.matcher(next)
-            if (!matcher.find() || matcher.groupCount() != 1) {
-                return null
-            }
-            return try {
-                Integer.parseInt(matcher.group(1))
-            } catch (ex: NumberFormatException) {
-                Log.e("Parse", "cannot parse next page from " + next)
-                null
-            }
-
-        }
-
-    companion object {
-        private val LINK_PATTERN = Pattern.compile("<([^>]*)>[\\s]*;[\\s]*rel=\"([a-zA-Z0-9]+)\"")
-        private val PAGE_PATTERN = Pattern.compile("page=(\\d)+")
-        private val NEXT_LINK = "next"
-    }
-}
-*/
