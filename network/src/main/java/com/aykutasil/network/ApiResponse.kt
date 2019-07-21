a3edd2950a5ebdd4d933b8046ab47d4a6ff1aed8
Aykut Asil <aykuttasil@gmail.com>
2019-07-21 23:57:07 +0300

    ApiResponse refactoring

--- network/src/main/java/com/aykutasil/network/ApiResponse.kt
+++ network/src/main/java/com/aykutasil/network/ApiResponse.kt
@@ -16,7 +16,6 @@
 package com.aykutasil.network
 
 import retrofit2.Response
-import java.util.regex.Pattern
 
 /**
  * Common class used by API responses.
@@ -35,10 +34,7 @@ sealed class ApiResponse<T> {
         if (body == null || response.code() == 204) {
           ApiEmptyResponse()
         } else {
-          ApiSuccessResponse(
-            body = body,
-            linkHeader = response.headers().get("link")
-          )
+          ApiSuccessResponse(body)
         }
       } else {
         val msg = response.errorBody()?.string()
@@ -53,54 +49,8 @@ sealed class ApiResponse<T> {
   }
 }
 
-/**
- * separate class for HTTP 204 resposes so that we can make ApiSuccessResponse's body non-null.
- */
 class ApiEmptyResponse<T> : ApiResponse<T>()
 
-data class ApiSuccessResponse<T>(
-  val body: T,
-  val links: Map<String, String>
-) : ApiResponse<T>() {
-  constructor(body: T, linkHeader: String?) : this(
-    body = body,
-    links = linkHeader?.extractLinks() ?: emptyMap()
-  )
-
-  val nextPage: Int? by lazy(LazyThreadSafetyMode.NONE) {
-    links[NEXT_LINK]?.let { next ->
-      val matcher = PAGE_PATTERN.matcher(next)
-      if (!matcher.find() || matcher.groupCount() != 1) {
-        null
-      } else {
-        try {
-          Integer.parseInt(matcher.group(1))
-        } catch (ex: NumberFormatException) {
-          // Timber.w("cannot parse next page from %s", next)
-          null
-        }
-      }
-    }
-  }
-
-  companion object {
-    private val LINK_PATTERN = Pattern.compile("<([^>]*)>[\\s]*;[\\s]*rel=\"([a-zA-Z0-9]+)\"")
-    private val PAGE_PATTERN = Pattern.compile("\\bpage=(\\d+)")
-    private const val NEXT_LINK = "next"
-
-    private fun String.extractLinks(): Map<String, String> {
-      val links = mutableMapOf<String, String>()
-      val matcher = LINK_PATTERN.matcher(this)
-
-      while (matcher.find()) {
-        val count = matcher.groupCount()
-        if (count == 2) {
-          links[matcher.group(2)] = matcher.group(1)
-        }
-      }
-      return links
-    }
-  }
-}
+data class ApiSuccessResponse<T>(val body: T) : ApiResponse<T>()
 
 data class ApiErrorResponse<T>(val errorMessage: String) : ApiResponse<T>()
