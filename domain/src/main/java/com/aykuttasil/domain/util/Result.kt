2c1801466709ff62725149e47484e81c6b2a9ef8
Aykut Asil <aykuttasil@gmail.com>
2020-06-24 23:53:36 +0300

    Update Result.kt

--- domain/src/main/java/com/aykuttasil/domain/util/Result.kt
+++ domain/src/main/java/com/aykuttasil/domain/util/Result.kt
@@ -7,13 +7,13 @@ package com.aykuttasil.domain.util
 sealed class Result<out T : Any> {
 
   data class Success<out T : Any>(val data: T) : Result<T>()
-  data class Error(val msg: String, val error: Throwable? = null) : Result<Nothing>()
+  data class Error(val error: Throwable) : Result<Nothing>()
   object Loading : Result<Nothing>()
 
   override fun toString(): String {
     return when (this) {
       is Success<*> -> "Success[data=$data]"
-      is Error -> "Error[exceptionMessage=$msg , exception=${error?.message}]"
+      is Error -> "Error[exception=${error.message ?: "Error"}]"
       Loading -> "Loading"
     }
   }
